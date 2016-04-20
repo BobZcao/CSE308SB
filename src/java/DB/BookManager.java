9aff13c4463cebb915fe77aa0a94811198a4d799
yongbinchen <yongbinchen@stonybrook.edu>
2016-04-20 11:32:24 -0400

    returnbokmanager

--- src/java/DB/BookManager.java
+++ src/java/DB/BookManager.java
@@ -7,6 +7,8 @@ package DB;
 
 import Model.Book.Book;
 import Model.Book.Borrow;
+import Model.Book.BorrowPK;
+import Model.Person.Account;
 import Model.Person.Member;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -21,7 +23,7 @@ public class BookManager {
     private static EntityManagerFactory bookFactory=Persistence.createEntityManagerFactory(PERSISTENCE_BOOK);
     private static final String PERSISTENCE_BORROW="Borrow";
     private static EntityManagerFactory borrowFactory=Persistence.createEntityManagerFactory(PERSISTENCE_BORROW);
-    
+ 
     
     public static void persistBook(Book book){
         EntityManager em = bookFactory.createEntityManager();
@@ -36,6 +38,20 @@ public class BookManager {
         em.close();
         return book;
     }
+     public static void persistReturn(Borrow borrow){
+        EntityManager em = borrowFactory.createEntityManager();
+        BorrowPK borrowPK=new BorrowPK();
+        borrowPK.setBook(borrow.getBook1().getIsbn());
+        borrowPK.setUser(borrow.getAccount().getUserName());
+        Borrow borrowFind=em.find(Borrow.class,borrowPK);
+        em.getTransaction().begin();
+        em.persist(borrowFind);
+        em.getTransaction().commit();
+        em.close();
+        persistBook(borrow.getBook1());
+        PersonManager.persistAccount(borrow.getAccount());
+    }
+    
     public static void persistBorrow(Borrow borrow){
         EntityManager em = borrowFactory.createEntityManager();
         em.getTransaction().begin();
